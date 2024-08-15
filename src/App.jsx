7746fab2dd59b26b96b8766c7c11e513eda78245
gabriela balieiro <gabibalieirosantos@gmail.com>
2024-08-15 14:09:06 +0000

    estilizaçao do login

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function  App() {
       <Route path='/singup' element={<SingUp/>}/>
       <Route path='/login' element={<Login/>}/>
       </Routes>
-</BrowserRouter>
+    </BrowserRouter>
   )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,42 +14,42 @@ export default function Singup() {
                 <button className='text-purple text-sm font-semibold border-solid border-2 border-indigo-500 rounded px-5'>Login</button>
             </div>
 
-            <div className='text-center my-6'>
+            <div className='text-center mb-6'>
             <img src={imgRobo} alt="Imagem de um robô" />
             </div>
           
         
                 <div className='text-center'>
-                    <h1 className='font-titulo'>Bem vindo ao</h1>
-                    <img src={logoSite} alt="Logo da marca Robô Kids" className='flex justify-center'/>
-
-                <p className='text-light-grey text-center my-4 text-base'>Crie a sua conta</p>
+                    <h1 className='font-titulo  mb-4 text-center'>Bem vindo ao</h1>
+                    <img src={logoSite} alt="Logo da marca Robô Kids" className='flex justify-center mr-10'/>
+                    </div>
+                <p className='text--grey text-center my-4 text-base '>Crie a sua conta:</p>
                 
                 <div className='max-w-sm mx-auto '>
                     <label htmlFor="text" className='block mb-2'>Nome:</label>
-                    <input type="text" placeholder='Nome de usuário' className='placeholder:font-inter placeholder:text-xs placeholder:text-light-grey block w-full p-2 border border-gray-300 rounded'/>
+                    <input type="text" placeholder='Nome de usuário' className='placeholder:font-inter placeholder:text-sm placeholder:text-grey block w-full p-2 border border-purple-300 rounded '/>
                 
 
                 <div>
                     <label htmlFor="email" className='block mt-4 mb-2'>E-mail:</label>
-                    <input type="text" placeholder='ex:robokids@gmail.com' className='block w-full p-2 border border-gray-300 rounded'/>
+                    <input type="text" placeholder='ex:robokids@gmail.com' className='block w-full p-2 border-2 border-purple-300 rounded'/>
                 </div>
 
                 <div>
-                    <label htmlFor="password" className='block mt-4 mb-2'>Senha:</label>
-                    <input type="text" placeholder='********' className='block w-full p-2 border border-gray-300 rounded' />
+                    <label htmlFor="password" className='block mt-4 mb-2 '>Senha:</label>
+                    <input type="text" placeholder='********' className='block w-full p-2 border-solid border-2 border-purple rounded' />
                 </div>
-                </div>
+                
 
                 <div className='flex justify-center mt-8 pl-6'>
-                    <button className='text-white bg-purple font-semibold border-solid border-2 border-indigo-500 rounded px-5'>Criar</button>
+                    <button className='text-white bg-purple font-semibold border-solid border-2 border-indigo-500 rounded px-5 hover:bg-white hover:text-purple hover:border-purple'>Criar</button>
                 </div>
 
                 <div className='flex justify-center space-x-6 mt-10'>
-                    <p className='text-light-grey mr-6 mb-10'>Crie uma conta com</p>
-                    <img src={iconGoogle} alt="Logo do Google" className='mr-2 mb-10 cursor-pointer' />
-                    <img src={iconFace} alt="Logo do Facebook" className='mr-2 mb-10 cursor-pointer' />
-                    <img src={iconApple} alt="Logo da Apple" className='mr-2 mb-10 cursor-pointer'/>
+                    <p className='text-gray mr-2 mb-10 p-3'>Crie uma conta com</p>
+                    <img src={iconGoogle} alt="Logo do Google" className='mr-2 mb-10 cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110' />
+                    <img src={iconFace} alt="Logo do Facebook" className='mr-2 mb-10 cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110' />
+                    <img src={iconApple} alt="Logo da Apple" className='mr-2 mb-11 cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110'/>
                 </div>
             </div>
         </main>
